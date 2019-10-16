22f4546e45b731191bad5b64636acb41806bd1d7
KennethRyu <xlong.xyi@163.com>
2019-10-16 17:38:12 +0800

    初始化

---
+++ src/TaxServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ryu\EveSeAT\Tax;
+
+//use Illuminate\Support\ServiceProvider;
+
+//class TaxServiceProvider extends ServiceProvider
+class TaxServiceProvider
+{
+    public function index(){
+        echo 123;
+    }
+//    /**
+//     * Bootstrap the application services.
+//     *
+//     * @return void
+//     */
+//    public function boot()
+//    {
+//        //
+//    }
+//
+//    /**
+//     * Register the application services.
+//     *
+//     * @return void
+//     */
+//    public function register()
+//    {
+//        //
+//    }
+}
---
+++ src/lang/en/tax.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mutterschiff
+ * Date: 12.02.2018
+ * Time: 00:15.
+ */
+
+return [
+
+    'seat_groups'           => 'SeAT Groups',
+    'seat_groups_dashboard' => 'Dashboard',
+
+    'seat_groups_autogroup' => 'Auto SeAT Groups',
+    'seat_groups_autogroup_description' => 'To these SeAT Groups you are automatically assigned based on your corporation membership',
+
+    'seat_groups_opengroup' => 'Open SeAT Groups',
+    'seat_groups_opengroup_description' => 'In these SeAT Groups you can freely opt-in and opt-out as you wish.',
+
+    'seat_groups_managedgroup'             => 'Managed SeAT Groups',
+    'seat_groups_managedgroup_description' => 'Here you can apply for certain groups. The managers of this group will approve or deny your request.',
+    'seat_groups_manager'                  => 'Manager',
+    'seat_join_opengroup'                  => 'Opt in',
+    'seat_leave_opengroup'                 => 'Opt out',
+    'seat_leave_waitlist'                  => 'Leave waitlist',
+    'seat_join_waitlist'                   => 'Apply',
+    'seat_leave_managedgroup'              => 'Leave',
+    'seat_deny_managedgroup'               => 'Reject',
+    'seat_accept_managedgroup'             => 'Accept',
+    'seat_removefrom_managedgroup'         => 'Remove',
+    'manage_members'                       => 'Manage Members',
+    'available_seatgroups'                 => 'Available SeAT Groups as Manager',
+
+    'seat_groups_hiddengroup' => 'Hidden SeAT Group',
+    'seat_groups_user'        => 'User',
+    'seat_groups_adduser'     => 'Add User',
+
+    'seat_groups_create'      => 'Create',
+    'seat_groups_create_new'  => 'Create a new SeAT Group',
+    'seat_groups_name'        => 'Name',
+    'seat_groups_description' => 'SeAT Group Description',
+    'seat_groups_type'        => 'Select SeAT Group Type',
+    'seat_groups_role'        => 'Select corresponding SeAT Role',
+    'seat_groups_add'         => 'Add new SeAT Group',
+
+    'seat_groups_edit'    => 'Add new SeAT Group',
+    'seat_groups_editing' => 'Editing',
+    'seat_groups_update'  => 'Update SeAT Group',
+
+    'seat_groups_about' => 'About',
+
+    'available_alliances' => 'Available Alliances',
+    'add_alliance'        => 'Add Alliance',
+
+    //Event Log
+    'event' => 'Event',
+
+];
