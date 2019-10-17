4133271caf302fbbfb45fdcf3fecc13ed8722b3f
KennethRyu <xlong.xyi@163.com>
2019-10-17 11:03:42 +0800

    修改

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -30,57 +30,39 @@ Route::group([
     'middleware' => ['web', 'auth', 'bouncer:seatgroups.view'],
 ], function () {
 
+    Route::get('/', [
+        'as'   => 'billing.view',
+        'uses' => 'BillingController@getLiveBillingView',
+    ]);
 
+    Route::get('/alliance/{alliance_id}', [
+        'as'   => 'billing.allianceview',
+        'uses' => 'BillingController@getLiveBillingView',
+    ]);
 
-});
+    Route::get('/settings', [
+        'as'   => 'billing.settings',
+        'uses' => 'BillingController@getBillingSettings',
+    ]);
 
-Route::group([
-    'namespace' => 'Denngarr\Seat\Billing\Http\Controllers',
-    'prefix' => 'billing'
-], function () {
-    Route::group([
-        'middleware' => ['web', 'auth'],
-    ], function () {
-        Route::get('/', [
-            'as'   => 'billing.view',
-            'uses' => 'BillingController@getLiveBillingView',
-            'middleware' => 'bouncer:billing.view'
-        ]);
+    Route::post('/settings', [
+        'as'   => 'billing.savesettings',
+        'uses' => 'BillingController@saveBillingSettings',
+    ]);
 
-        Route::get('/alliance/{alliance_id}', [
-            'as'   => 'billing.allianceview',
-            'uses' => 'BillingController@getLiveBillingView',
-            'middleware' => 'bouncer:billing.view'
-        ]);
+    Route::get('/getindbilling/{id}', [
+        'as'   => 'billing.getindbilling',
+        'uses' => 'BillingController@getUserBilling',
+    ]);
 
-        Route::get('/settings', [
-            'as'   => 'billing.settings',
-            'uses' => 'BillingController@getBillingSettings',
-            'middleware' => 'bouncer:billing.settings'
-        ]);
+    Route::get('/pastbilling/{year}/{month}', [
+        'as'   => 'billing.pastbilling',
+        'uses' => 'BillingController@previousBillingCycle',
+    ]);
 
-        Route::post('/settings', [
-            'as'   => 'billing.savesettings',
-            'uses' => 'BillingController@saveBillingSettings',
-            'middleware' => 'bouncer:billing.settings'
-        ]);
+    Route::get('/getindpastbilling/{id}/{year}/{month}', [
+        'as'   => 'billing.getindbilling',
+        'uses' => 'BillingController@getPastUserBilling',
+    ]);
 
-        Route::get('/getindbilling/{id}', [
-            'as'   => 'billing.getindbilling',
-            'uses' => 'BillingController@getUserBilling',
-            'middleware' => 'bouncer:billing.view'
-        ]);
-
-        Route::get('/pastbilling/{year}/{month}', [
-            'as'   => 'billing.pastbilling',
-            'uses' => 'BillingController@previousBillingCycle',
-            'middleware' => 'bouncer:billing.view'
-        ]);
-
-        Route::get('/getindpastbilling/{id}/{year}/{month}', [
-            'as'   => 'billing.getindbilling',
-            'uses' => 'BillingController@getPastUserBilling',
-            'middleware' => 'bouncer:billing.view'
-        ]);
-    });
 });
